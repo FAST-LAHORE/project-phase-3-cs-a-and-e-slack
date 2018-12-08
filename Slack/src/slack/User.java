af41f3907afd52640dddd34c343ee08606242d49
SairaSaddique <SairaSaddique@users.noreply.github.com>
2018-12-08 17:03:28 +0500

    Revert "Channel messages updated"
    This reverts commit d680cf7e18dc466e2d087be8ee7b326fef622b13.

---
+++ Slack/src/slack/User.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slack;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import static slack.Slack.conn;
+import static slack.Slack.obj;
+
+/**
+ *
+ * @author mac
+ */
+public class User 
+{
+ private String name;
+ private String email;
+ private String password;
+ ArrayList<String> created;
+ ArrayList<String> Joined;
+ 
+
+ User(String n, String p) throws SQLException
+ {
+     email=n; 
+     password=p;
+     Joined=obj.myWorkspace(email);
+     //name lelo database se
+ }
+ 
+ boolean checkLogin() throws SQLException
+    {
+     ResultSet rs=  obj.checkLogin(email, password);
+     if(rs.next())
+     {
+         name=rs.getString("Name");
+         return true;
+     }
+     else
+         return false;
+    }
+ 
+ 
+    boolean Signup(String n) throws SQLException
+    {
+       name=n;
+       return obj.Signup(name, email, password);
+    }
+ 
+    String getName()
+    {
+        return email;
+    }
+    
+    ArrayList<String> myWorkspace() throws SQLException
+    {
+        Joined=obj.myWorkspace(email);
+       return Joined;
+    }
+    
+    void addCreatedWS(String obj)
+    {
+        created.add(obj);
+    }
+    
+    void addJoinedWS(String obj)
+    {
+        Joined.add(obj);
+    }
+    
+}
---
+++ Slack/src/slack/Workspace.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slack;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import static slack.Slack.conn;
+import static slack.Slack.obj;
+
+/**
+ *
+ * @author mac
+ */
+public class Workspace 
+{
+    private String name;
+    private String password;
+    private String creator;
+    ArrayList<String> users=new ArrayList<>();
+    ArrayList<Chats> chats;
+    
+    Workspace(String n, String c,String p) throws SQLException //creates new workspace
+    {
+        name=n;
+        password=p;
+        creator=c;
+        GetMembers();
+       
+    }
+    
+    Workspace(String n) throws SQLException
+    {
+        name =n;
+        ResultSet rs = obj.GetWorkspaceDetails(n);
+        if(rs.next())
+        {
+            creator = rs.getString("creator");
+            password = rs.getString("password");
+        }
+        GetMembers();
+    }
+    void GetMembers() throws SQLException
+    {
+        ResultSet rs = obj.GetWorkspaceMembers(name);
+        while(rs.next())
+        {
+            users.add(rs.getString("USERNAME"));
+        }
+       users.remove(creator);
+    }
+    boolean createWorkspace() throws SQLException
+    {
+        return obj.createWorkspace(name, creator, password);
+    }
+    
+    void addUser(String a)
+    {
+        users.add(a);
+    }
+    
+    ArrayList<String> getUsers() throws SQLException
+    {
+        if(users!=null)
+            users.clear();
+        GetMembers();
+        return users;
+    }
+    
+    
+    
+}
